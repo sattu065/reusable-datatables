bfc8faeb163a1daf708dd6a6e1e5a4134a5100c1
satyendra <raghuwanshisatyendra0@gmail.com>
2022-04-04 15:07:29 +0530

    message

--- force-app/main/default/lwc/showRecentItems/showRecentItems.js
+++ force-app/main/default/lwc/showRecentItems/showRecentItems.js
@@ -1,7 +1,20 @@
 import { LightningElement, track,api } from 'lwc';
+import {loadStyle} from 'lightning/platformResourceLoader';
+import COLORS from '@salesforce/resourceUrl/colors';
 
 export default class ShowRecentItems extends LightningElement {
     @api records;
     @api columns;
     @api title;
+    isCssLoaded = false;
+
+    renderedCallback(){ 
+        if(this.isCssLoaded) return;
+        this.isCssLoaded = true;
+        loadStyle(this, COLORS).then(()=>{
+            console.log("Loaded Successfully");
+        }).catch(error=>{ 
+            console.error("Error in loading the colors");
+        })
+    }
 }
